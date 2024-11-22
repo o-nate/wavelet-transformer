81677b25fdbfbcaa7ecacdea3e3e4d6e846912a9
le-nate <99023298+o-nate@users.noreply.github.com>
2024-11-22 11:28:02 +0100

    add: legend optimized for mobile

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import logging
 import sys
 
 import streamlit as st
-import pandas as pd
 import plotly.graph_objects as go
 
 from src.utils.file_helpers import convert_to_dataframe
@@ -32,7 +31,6 @@ if uploaded_files:
 
     for uploaded_file in uploaded_files:
         df = convert_to_dataframe(uploaded_file)
-        logger.debug("df columns: %s", df.columns)
 
         # Add trace to plot
         fig.add_trace(
@@ -46,9 +44,22 @@ if uploaded_files:
         title=f"Data Visualization of: {' '.join(file_dict.keys())}",
         xaxis_title="Date",
         yaxis_title="Value",
+        legend={
+            "orientation": "h",
+            "entrywidthmode": "fraction",
+            "entrywidth": 0.8,
+            "yanchor": "top",
+            "y": 1.15,
+            "xanchor": "right",
+            "x": 1,
+        },
     )
 
     # Display plot
     st.plotly_chart(fig)
 else:
-    st.write("Upload a file to begin")
+    st.write("Upload a spreadsheet file to begin.")
+    st.write(
+        """Please, ensure that each file has only two columns: 
+        the **date column on the left** and the **value column on the right**."""
+    )
