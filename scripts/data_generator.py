d24e715c373bc362a5a70b91ecb7a5f8ecf4a628
le-nate <99023298+o-nate@users.noreply.github.com>
2025-01-24 11:26:38 +0100

    change: data

--- scripts/data_generator.py
+++ scripts/data_generator.py
@@ -16,8 +16,8 @@ logger = get_logger(__name__)
 parent_directory = Path(__file__).parents[1]
 export_directory = parent_directory / "data"
 
-DATA_REQUESTED = "CPALTT01BRM659N"
-FILE_NAME = "brazil_inflation"
+DATA_REQUESTED = ids.US_DURABLES_CONSUMPTION
+FILE_NAME = "durables"
 
 # %% [markdown]
 # Retrieve data via API
